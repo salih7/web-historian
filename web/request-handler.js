abc12c77c0e8abbe2881420b5d3be090dcfdd616
Hack Reactor Student <student@hackreactor.com>
2017-04-19 19:48:57 -0700

    Implement logic for callback helper functions

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,62 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var path = require('path');
+var querystring = require('querystring');
 // require more modules/folders here!
 
+var headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10,
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  
+  var ep = {
+    //''
+  };
+
+  if (req.method === 'GET') {  
+    // console.log(req.url)
+    if (req.url === '/') {  
+      var file = archive.paths.siteAssets + '/index.html'; 
+      res.writeHead(200, headers);
+      fs.readFile(file, 'utf8', function(err, html) {
+        if (err) { throw err; }
+        res.end(html);
+      });    
+    } else {
+      fs.readFile(archive.paths.list, 'utf8', function(err, data) {
+        if (err) { throw err; }
+        fs.readdir(archive.paths.archivedSites, (err, files) => {
+          if (files.some(file => file === req.url.slice(1))) {
+            var path2 = archive.paths.archivedSites + req.url;
+            fs.readFile(path2, 'utf8', function(err, html) {
+              if (err) { throw err; }
+              res.writeHead(200, headers);
+              res.end(html);
+            });    
+          } else {
+            res.writeHead(404, 'Not Found!');
+            res.end();
+          }
+        });
+        // if(data.indexOf(req.url.slice(1)) >= 0) {
+        //   // TODO
+        // } else {
+        //   res.writeHead(404, 'Not Found!');
+        //   res.end();
+        // }
+      });   
+    }
+
+  } else if (req.method === 'POST') {
+    fs.appendFile(archive.paths.list, req.url.slice(1) + '\n'); 
+    res.writeHead(302, 'Done');
+    res.end(); 
+  }
+  
+      //res.end(archive.paths.list);
 };
